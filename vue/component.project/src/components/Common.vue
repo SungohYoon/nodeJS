f2f1ca874817eb7f41d629955106bffa5cea37d5
SungohYoon <nsa30003@gmail.com>
2023-11-30 17:54:28 +0900

    23-11-30 vue.js

---
+++ vue/component.project/src/components/Common.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: [],
+  data() {
+    return {
+      today: new Data(),
+    };
+  },
+  computed: {
+    AfterYear() {
+      return this.today.setFullYear(this.today.getFullYear() + 1);
+    },
+  },
+  methods: {},
+  watch: {},
+  created() {},
+  mounted() {},
+};
+</script>
+<style></style>
---
+++ vue/component.project/src/views/DataBinding.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>{{ title + ' !!!' }}</h1>
+    <h2 v-bind:name="'sample'" v-text="title"></h2>
+    <p v-html="tagList" />
+    <p v-text="tagList" />
+    <hr />
+    <input type="text" v-model="valueModel" />
+    <p>{{ typeof valueModel }}</p>
+    <input type="number" v-model.lazy="numberModel" />
+    <p>{{ 10000 + numberModel }}</p>
+    <select v-model="selectModel">
+      <option value="jjaJang">간짜장</option>
+      <option value="jjamBbong">짬뽕</option>
+    </select>
+    <p>{{ selectModel }}</p>
+    <textarea v-model="textModel"></textarea>
+    <hr />
+    <input type="checkbox" v-model="cnData" true-value="남자" false-value="여자" />
+    <p>{{ cnData }}</p>
+    <div>
+      <input type="checkbox" value="서울" v-model="city" />
+      <input type="checkbox" value="구미" v-model="city" />
+      <p>{{ city }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello, Vue.js',
+      tagList: '<strong>Today is ... </strong>',
+      valueModel: 'korea',
+      numberModel: 0,
+      selectModel: '',
+      textModel: '택견불여일타쌍피',
+      cnData: '',
+      city: '',
+    };
+  },
+};
+</script>
